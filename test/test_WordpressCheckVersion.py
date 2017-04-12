397de48a939074c3c52879a34ac43c75dd0eb74b
GoldraK <goldrak@gmail.com>
2017-04-12 07:36:05 +0200

    Fix close files, before finish test

--- test/test_WordpressCheckVersion.py
+++ test/test_WordpressCheckVersion.py
@@ -7,81 +7,77 @@ from wordpresscheckversion import WordpressCheckVersion
 class WCVTests(unittest.TestCase):
     def test_Readme(self):
 
-        path = os.path.join(os.path.dirname(__file__), 'webfiles/readme.html')
-        testdata = open(path).read()
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/readme.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
         v = w.getReadme(testdata)
-
+        file.close()
         self.assertEqual(v, '4.7')
 
     def test_Readme_no_version(self):
 
-        path = os.path.join(os.path.dirname(__file__), 'webfiles/readme_no_version.html')
-        testdata = open(path).read()
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/readme_no_version.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
         v = w.getReadme(testdata)
-
+        file.close()
         self.assertFalse(v)
 
     def test_Admin(self):
-
-        path = os.path.join(os.path.dirname(__file__), 'webfiles/wp-login.html')
-        testdata = open(path).read()
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/wp-login.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
         v = w.getAdmin(testdata)
-
+        file.close()
         self.assertEqual(v, '4.7.3')
 
     def test_Admin_no_version(self):
 
-        path = os.path.join(os.path.dirname(__file__), 'webfiles/wp-login_no_version.html')
-        testdata = open(path).read()
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/wp-login_no_version.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
         v = w.getAdmin(testdata)
-
+        file.close()
         self.assertFalse(v)
 
-
     def test_GeneratorSiteFeed(self):
-
-        path = os.path.join(os.path.dirname(__file__), 'webfiles/feed.xml')
-        testdata = open(path).read()
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/feed.xml'))
+        testdata = file.read()
         w = WordpressCheckVersion()
         v = w.getGeneratorFeed(testdata)
-
-
-
+        file.close()
         self.assertEqual(v, '4.7.3')
 
-
     def test_GeneratorSiteFeed_no_version(self):
-
-        path = os.path.join(os.path.dirname(__file__), 'webfiles/feed_no_version.xml')
-        testdata = open(path).read()
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/feed_no_version.xml'))
+        testdata = file.read()
         w = WordpressCheckVersion()
         v = w.getGeneratorFeed(testdata)
-
+        file.close()
         self.assertFalse(v)
 
     def test_GeneratorSite(self):
-
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/index.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
-        v = w.getGeneratorSite('<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost.example/xmlrpc.php?rsd" /><link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost.example/wp-includes/wlwmanifest.xml" /> <meta name="generator" content="WordPress 4.7.3" /><link rel="canonical" href="http://localhost.example/" /><link rel="shortlink" href="http://localhost.example/" />')
-
+        v = w.getGeneratorSite(testdata)
+        file.close()
         self.assertEqual(v, '4.7.3')
 
     def test_GeneratorSite_no_version(self):
-
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/index_no_version.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
-        v = w.getGeneratorSite('<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost.example/xmlrpc.php?rsd" /><link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost.example/wp-includes/wlwmanifest.xml" /> <meta name="generator" content="WordPress" /><link rel="canonical" href="http://localhost.example/" /><link rel="shortlink" href="http://localhost.example/" />')
-
+        v = w.getGeneratorSite(testdata)
+        file.close()
         self.assertFalse(v)
 
     def test_GeneratorSite_no_generator(self):
-
+        file = open(os.path.join(os.path.dirname(__file__), 'webfiles/index_no_generator.html'))
+        testdata = file.read()
         w = WordpressCheckVersion()
-        v = w.getGeneratorSite('<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost.example/xmlrpc.php?rsd" /><link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost.example/wp-includes/wlwmanifest.xml" /><link rel="canonical" href="http://localhost.example/" /><link rel="shortlink" href="http://localhost.example/" />')
-
+        v = w.getGeneratorSite(testdata)
+        file.close()
         self.assertFalse(v)
 
 
