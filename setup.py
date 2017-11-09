23afe8d7520be739a73745b360067ee1938561d8
Goldrak <goldrak@gmail.com>
2017-11-09 23:16:22 +0100

    Revision of pep8

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='WordpressCheckVersion',
     version='0.1.0',
     packages=['wordpresscheckversion'],
-    install_requires=['bs4', 'beautifulsoup4','lxml'],
+    install_requires=['bs4', 'beautifulsoup4', 'lxml'],
     url='https://github.com/securipy/WordpressCheckVersion',
     license='GPL',
     author='goldrak',
     author_email='goldrak@gmail.com',
     description='Python class to process html to detect wordpress version',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
